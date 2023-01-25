e5b525766e6daa9f460f1231212a2c2ae02b4495
Didier Barradas Bautista <didier.barradas@gmail.com>
2023-01-25 09:49:58 +0300

    code restructure and reorganization of the folders

--- install_miniconda_shaheen/check_time.sh
+++ install_miniconda_shaheen/check_time.sh
@@ -6,7 +6,9 @@ export CONDA_PKGS_DIRS=$PWD/conda_cache
 # activate conda base from the command line
 source $PWD/miniconda3/bin/activate
 
-# use time to measure the time taken by conda
+#clear the cache with mamba clean
 mamba clean --all -y
+
+# use time to measure the time taken by conda
 time  mamba create -p $PWD/env -c conda-forge pandas python scikit-learn -d
 echo "conda create time: $SECONDS seconds"
