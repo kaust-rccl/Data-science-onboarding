e4138799df26e1e4ec5e246bfd8975af325c57e8
Didier Barradas Bautista <didier.barradas@gmail.com>
2023-08-27 09:55:27 +0300

    added miniforge installer

---
+++ install_miniconda_ibex/install-miniconda-ibex.sh
@@ -0,0 +1,26 @@
+#!/bin/bash --login
+# entire script fails if a single command fails
+set -e
+# define the folder to install 
+export PREFIX=$HOME/miniconda3
+# or define a path to install
+# export PREFIX=/ibex/scratch/$USER/miniconda3
+
+# create the folder if it doesn't exist
+mkdir -p $PREFIX
+
+# download and install miniforge
+curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
+bash Mambaforge-$(uname)-$(uname -m).sh -b -p $PREFIX
+
+# once installed, we need to activate the base environment
+source $PREFIX/bin/activate
+
+# update conda to most recent version (if necessary)
+conda update --name base --channel defaults --yes conda
+
+# make sure that base environment is not active by default
+conda config --set auto_activate_base false
+
+# remove the installer
+rm Mambaforge-$(uname)-$(uname -m).sh
