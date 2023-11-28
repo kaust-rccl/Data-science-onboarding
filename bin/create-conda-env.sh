2c615beee976229de081ca53c47d3e77daa19eeb
Didier Barradas Bautista <didier.barradas@gmail.com>
2023-11-28 10:14:13 +0300

    Update to the repo for the next tutorial

--- bin/create-conda-env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash --login
-# entire script fails if a single command fails
-set -e
-
-# create the conda environment
-export ENV_PREFIX=$PWD/env
-mamba env create --prefix $ENV_PREFIX --file environment.yml --force
-
