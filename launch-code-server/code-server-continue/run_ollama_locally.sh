6faaae7e6ae6dd48248de6b9ecae02c257c06ee5
D-Barradas <didier.barradas@gmail.com>
2025-01-07 16:58:07 +0300

    download and run ollama in the bg

---
+++ launch-code-server/code-server-continue/run_ollama_locally.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Define variables
+OLLAMA_URL="https://ollama.com/download/ollama-linux-amd64.tgz"
+OLLAMA_TAR="ollama-linux-amd64.tgz"
+OLLAMA_BINARY="$(pwd)/bin/ollama"
+
+
+# Check if Ollama binary exists
+if [ ! -f "$OLLAMA_BINARY" ]; then
+    echo "Ollama binary not found. Downloading..."
+    curl -L $OLLAMA_URL -o $OLLAMA_TAR
+
+    echo "Extracting Ollama binary..."
+    tar -xzf $OLLAMA_TAR
+
+    echo "Cleaning up..."
+    rm $OLLAMA_TAR
+fi
+
+# # Run Ollama binary in the background
+echo "Running Ollama binary in the background..."
+nohup $OLLAMA_BINARY serve &
+
+# # Verify that Ollama is running
+echo "Verifying Ollama is running..."
+$OLLAMA_BINARY -v
+
+# # download qwnen2.5
+echo "Downloading qwnen2.5..."
+$OLLAMA_BINARY pull qwnen2.5-coder:1.5b
+
+# # download pull llama3.1
+$OLLAMA_BINARY pull llama3.1:8b
+$OLLAMA_BINARY pull llama3.2:3b
+
+
+echo "Setup complete."
